e310d532bcdb8704eb563be3969d992470a3dd61
Leonardo <leonardoduartevargas@gmail.com>
2019-01-28 09:27:54 -0200

    Export Data

--- Book Organizer.py
+++ Book Organizer.py
@@ -33,6 +33,18 @@ book_collection = []
 for b in collection:
     book_collection.append(Book(b[0],b[1],b[2]))
 
+
 organized_objects = organize(config_rule, book_collection)
+
+
+organized_collection = []
+for b in organized_objects:
+    organized_collection.append([b.title, b.author, b.year])
+
+with open('Organized Collection.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerows(organized_collection)
+f.close()
+
 
 input("Livros Organizados\nPressione qualquer tecla para continuar...")
